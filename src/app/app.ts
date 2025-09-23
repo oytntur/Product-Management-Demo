1ae85a73d85dadce98f5d2c469b165863074f384
Oytun TUR <turoytun0@gmail.com>
2025-09-23 15:40:34 +0300

    feat:added devextreme to project

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { DxButtonComponent } from 'devextreme-angular/ui/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DxButtonComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('sabahyildizi-interview-1');
