def8e1243a33e5c62f3280d19552151c81cf39bd
Oytun TUR <turoytun0@gmail.com>
2025-09-23 15:57:38 +0300

    feat:added login and register pages and routes for that pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    children: [
+      {
+        path: 'login',
+        loadComponent: () =>
+          import('./pages/auth/login/login.component').then((m) => m.LoginComponent),
+      },
+      {
+        path: 'register',
+        loadComponent: () =>
+          import('./pages/auth/register/register.component').then((m) => m.RegisterComponent),
+      },
+    ],
+  },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { DxButtonComponent } from 'devextreme-angular/ui/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, DxButtonComponent],
+  imports: [RouterOutlet, DxButtonComponent, RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
